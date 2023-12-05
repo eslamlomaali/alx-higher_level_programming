c3fdefcd1f68c45b35d568736bc00e1495b993c5
eslamlomaali <eslam@gmail.com>
2023-12-05 03:53:10 -0800

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""function that writes a string to a text file (UTF8)"""
+"""function that writes a string to a text file (UTF8)."""
 
 
 def write_file(filename="", text=""):
     """write content in UTF8 text file"""
-    with open(filename, "w" , encoding="utf-8") as fille:
-	return fille.write(text)
+    with open(filename, "w", encoding="utf-8") as fille:
+        return fille.write(text)
 
