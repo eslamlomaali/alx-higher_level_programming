8e7224a50fff62be6a9d17c73d776d4c58983d53
eslamlomaali <eslam@gmail.com>
2023-12-05 04:02:36 -0800

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""function that append a string to a text file (UTF8)."""
+"""function that append a string to a text file (UTF8).."""
 
 
 def append_write(filename="", text=""):
     """append content in UTF8 text file"""
     with open(filename, "a", encoding="utf-8") as fille:
-	return fille.write(text)
+        return fille.write(text)
