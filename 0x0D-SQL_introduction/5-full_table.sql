4a5076faeaf4aa398d322fc5ebd3531c7693e2f9
eslamlomaali <eslam@gmail.com>
2024-01-16 16:46:02 -0800

    done

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
---Write a script that prints the full desc of the table firsttable from the db
+-- Write script that prints the full desc of the table firsttable from the db
 SHOW CREATE TABLE `first_table`;
