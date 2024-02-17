80e5d488da57bc607a27b8a33e3f6dd4ae387fc8
eslamlomaali <eslam@gmail.com>
2024-02-17 13:18:17 +0000

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 
-Write a script that lists all states with a name starting 
+"""
+Write a script that lists all states with a name starting
 with N (upper N) from the database hbtn_0e_0_usa:
 
 """
