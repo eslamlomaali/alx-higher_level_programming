db238bb2f96db510f3df7388a7c1b46d7a28ce0b
eslamlomaali <eslam@gmail.com>
2024-03-01 11:22:38 +0200

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,8 +3,8 @@
 POST request to the passed URL with the email as a parameter
 and finally displays the body of the response.
 """
-  import sys
-  import requests
+import sys
+import requests
 
 
   if __name__ == "__main__":
