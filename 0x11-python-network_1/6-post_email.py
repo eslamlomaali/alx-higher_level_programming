635e973401525d689173cee2db23a7e8d004081f
eslamlomaali <eslam@gmail.com>
2024-03-01 11:20:11 +0200

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL and an email address, sends POST
-request to the passed URL with the email as a parameter,
+"""Write a Python script that takes in a URL and an email address, sends
+POST request to the passed URL with the email as a parameter
 and finally displays the body of the response.
 """
-import sys
-import urllib.request
+  import sys
+  import requests
 
-if __name__ == "__main__":
+
+  if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.post(url, data=value)
+    print(r.text)
