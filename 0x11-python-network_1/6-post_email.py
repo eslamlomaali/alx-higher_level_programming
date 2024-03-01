0c896fd9ec0bd8d1bd99a4c532cb59d6bd2ef96c
eslamlomaali <eslam@gmail.com>
2024-03-01 11:17:10 +0200

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a URL and an email address, sends POST
-request to the passed URL with the email as a parameter, and finally displays
-the body of the response.
+request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 """
 import sys
 import urllib.request
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as ans:
-        print(dict(ans.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
