add5707f8accc4a4af9b20b462eb03d349f38cb5
eslamlomaali <eslam@gmail.com>
2024-03-26 14:21:53 +0200

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const files = require('fs');
-files.readFile(process.argv[2], 'utf8', function (error, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+
 	  console.log(error || content);
 });
